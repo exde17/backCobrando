7621efa3cb62ea7dab0c51fe852e1d6755eff245
mario <mabg0630@gmail.com>
2023-07-15 13:23:44 -0500

    cambio

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -62,11 +62,11 @@ export class AuthService {
     if (!bcrypt.compareSync(loginUserDto.password, user.password))
       throw new UnauthorizedException('el password no es correcto');
 
-    return ({data:{
+    return ({
       id: user.id,
       email: user.email,
       token: this.getjwtToken({ id: user.id }),
-    }})  
+    })  
 
   }
 
